e2284d857a07ba359dc33edd0bfd2f449266da2b
Denkaev <coraa@rambler.ru>
2022-12-01 07:32:07 +1200

    New metod for camera

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 10f;
+    private float speed = 25f;
     private Rigidbody2D rb;
     [SerializeField]
     private GameObject player;
@@ -23,33 +23,33 @@ public class CameraMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (GlobalVariables.FreeCam)
-        {
-            newtrans.x = player.transform.position.x + offset.x;
-            newtrans.y = player.transform.position.y + offset.y;
-            transform.position = newtrans;
-        }
+        //if (GlobalVariables.FreeCam)
+        //{
+        //    newtrans.x = player.transform.position.x + offset.x;
+        //    newtrans.y = player.transform.position.y + offset.y;
+        //    transform.position = newtrans;
+        //}
 
         //Ќадо разобрать почему не работает сразу , а только после движени€ мышкой
         //“еперь дергаетс€ делает сдвиг и возвращаетс€ на прежнюю позицию сразу , и движение по ос€м надо делать в общих координатах
         //“олько один сдвиг
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
-        //—лишком резко
-        if (Input.GetKeyDown(KeyCode.Q))
-        //            transform.rotation = Quaternion.Euler(0f, 0f, 10f);
-        //¬роде работает но надо тут ключ активировать а не само вращение делать
-        {
-            GlobalVariables.RotateTimer = 10;
-            GlobalVariables.RotateCam = 1f;
-        }
-        else if (GlobalVariables.RotateTimer > 0)
-            GlobalVariables.RotateTimer -= 1;
-        else if (Input.GetKeyDown(KeyCode.E))
-            transform.rotation = Quaternion.Euler(0f, 0f, -10f);
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            rb.velocity = new Vector3(0f, 0f, 10f * speed);
-        else if (Input.GetKeyDown(KeyCode.LeftControl))
-            rb.velocity = new Vector3(0f, 0f, -10f * speed);
+        //rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+        ////—лишком резко
+        //if (Input.GetKeyDown(KeyCode.Q))
+        ////            transform.rotation = Quaternion.Euler(0f, 0f, 10f);
+        ////¬роде работает но надо тут ключ активировать а не само вращение делать
+        //{
+        //    GlobalVariables.RotateTimer = 10;
+        //    GlobalVariables.RotateCam = 1f;
+        //}
+        //else if (GlobalVariables.RotateTimer > 0)
+        //    GlobalVariables.RotateTimer -= 1;
+        //else if (Input.GetKeyDown(KeyCode.E))
+        //    transform.rotation = Quaternion.Euler(0f, 0f, -10f);
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //    rb.velocity = new Vector3(0f, 0f, 10f * speed);
+        //else if (Input.GetKeyDown(KeyCode.LeftControl))
+        //    rb.velocity = new Vector3(0f, 0f, -10f * speed);
 
     }
 
@@ -72,12 +72,22 @@ public class CameraMove : MonoBehaviour
 
     private void Update()
     {
-        if (GlobalVariables.RotateTimer > 0)
-            transform.Rotate(0f, 0f, GlobalVariables.RotateCam, Space.World);
+        MoveCamera(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetAxis("Mouse ScrollWheel"));
+
+        //if (GlobalVariables.RotateTimer > 0)
+        //    transform.Rotate(0f, 0f, GlobalVariables.RotateCam, Space.World);
         //        //transform.rotation *= Quaternion.Euler(0f, 50f * Time.deltaTime, 0f);
         //        transform.rotation = Quaternion.Euler(0f, 0f , 100f * Time.deltaTime);
         //    //else if (Input.GetKey(KeyCode.E))
         //    ////transform.rotation *= Quaternion.Euler(0f, -50f * Time.deltaTime, 0f);
         //    //transform.rotation = Quaternion.Euler(0f, -50f * Time.deltaTime, 0f);
+    }
+
+    void MoveCamera(float x, float y, float z)
+    {
+        Vector3 movementAmount = new Vector3(x, y, z) * speed * Time.deltaTime;
+        transform.Translate(movementAmount);
+        if (!(z == 0))
+        transform.Translate(Vector3.forward * z);
     }
 }

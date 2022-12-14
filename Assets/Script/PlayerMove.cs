d8a44b7fc9087174d743bd558c3dfd0c8872d281
Denkaev <coraa@rambler.ru>
2022-12-15 08:39:34 +1200

    Last step

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField]
     private float speed = 1f;
     private Vector3 target;
-
-
+    //private Rigidbody2D rb;
+        
     void Start()
     {
         target = transform.position;
+        Debug.Log("Start");
+        //rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -30,4 +33,13 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Bum");
+        target = transform.position;
+        //rb.velocity = Vector3.zero;
+        //transform.position = Vector3.zero;
+    }
+    
 }

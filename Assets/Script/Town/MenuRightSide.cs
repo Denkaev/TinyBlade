ec7a728499e52da6eb1e4f941be4f02f3adc8bff
Denkaev <coraa@rambler.ru>
2023-01-01 10:34:34 +1200

    Up

---
+++ Assets/Script/Town/MenuRightSide.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuRightSide : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject farm;
+
+    public void PressButon()
+    {
+        //Пока включить выключить
+        //Потом анимация постройки , постройка , убрать кнопку из листа возможных построек
+        Invert(farm);
+    }
+
+
+    public void Invert(GameObject build)
+    {
+        build.SetActive(!build.activeSelf);
+    }
+}

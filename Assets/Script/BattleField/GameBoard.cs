8b2b9c2a9bdf0534d0af987fc7a53b1e060f808b
Denkaev <coraa@rambler.ru>
2023-02-16 08:08:01 +1200

    Board array[][]

--- Assets/Script/BattleField/GameBoard.cs
+++ Assets/Script/BattleField/GameBoard.cs
@@ -7,19 +7,23 @@ public class GameBoard : MonoBehaviour
     [SerializeField]
     GameTile tilePrefab = default;
     Vector2Int size;
-    GameTile[] tiles;
+    GameTile[][] tiles;
+    //GameTile[] tiles;
 
     public void Initialize(Vector2Int size)
     {
         this.size = size;
         ground.localScale = new Vector3(size.x, size.y, 1f);
         Vector2 offset = new Vector2((size.x - 1) * 0.5f, (size.y - 1) * 0.5f);
-        tiles = new GameTile[size.x * size.y];
+        tiles = new GameTile[size.y][];
+        //tiles = new GameTile[size.x * size.y];
         for (int i = 0, y = 0; y < size.y; y++)
         {
+            tiles[y] = new GameTile[size.x];
             for (int x = 0; x < size.x; x++, i++)
             {
-                GameTile tile = tiles[i] = Instantiate(tilePrefab);
+                GameTile tile = tiles[y][x] = Instantiate(tilePrefab);
+                //GameTile tile = tiles[i] = Instantiate(tilePrefab);
                 tile.transform.SetParent(transform, false);
                 tile.transform.localPosition = new Vector3(x - offset.x, 0f, y - offset.y);
             }

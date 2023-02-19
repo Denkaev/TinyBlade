9a934e24167e990e13ebea1063622e20b4e3009d
Denkaev <coraa@rambler.ru>
2023-02-20 08:40:21 +1200

    Up

--- Assets/Script/BattleField/GameBoard.cs
+++ Assets/Script/BattleField/GameBoard.cs
@@ -7,8 +7,8 @@ public class GameBoard : MonoBehaviour
     [SerializeField]
     GameTile tilePrefab = default;
     Vector2Int size;
+    //Ќедостаточно разделить на двумерный массив, надо хранить в нем тип тайла и при генерации добавить шанс применени€ каждого тайла
     GameTile[][] tiles;
-    //GameTile[] tiles;
 
     public void Initialize(Vector2Int size)
     {
@@ -16,14 +16,12 @@ public class GameBoard : MonoBehaviour
         ground.localScale = new Vector3(size.x, size.y, 1f);
         Vector2 offset = new Vector2((size.x - 1) * 0.5f, (size.y - 1) * 0.5f);
         tiles = new GameTile[size.y][];
-        //tiles = new GameTile[size.x * size.y];
         for (int i = 0, y = 0; y < size.y; y++)
         {
             tiles[y] = new GameTile[size.x];
             for (int x = 0; x < size.x; x++, i++)
             {
                 GameTile tile = tiles[y][x] = Instantiate(tilePrefab);
-                //GameTile tile = tiles[i] = Instantiate(tilePrefab);
                 tile.transform.SetParent(transform, false);
                 tile.transform.localPosition = new Vector3(x - offset.x, 0f, y - offset.y);
             }

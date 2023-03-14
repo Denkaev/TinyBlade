998c3a2357d36e5fa849047e6b3e23f9ed9d2e2e
Denkaev <coraa@rambler.ru>
2023-03-15 08:19:57 +1200

    Up

--- Assets/Script/BattleField/BFUnitDragDrop.cs
+++ Assets/Script/BattleField/BFUnitDragDrop.cs
@@ -18,7 +18,24 @@ public class BFUnitDragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHand
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        prefabCurrent = Instantiate(unitPrefab,rectTransform);
+        //prefabCurrent = Instantiate(unitPrefab,rectTransform);
+        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //потом разберусь с этим костылем
+        //Transform unitPos;
+        
+        //var unitPos = mousePosition;
+        //unitPos.x = mousePosition.x;
+        //unitPos.y = mousePosition.y;
+        //unitPos.z = mousePosition.y;
+        //mousePosition.z = 0;
+        //prefabCurrent = Instantiate(unitPrefab, (Transform)Input.mousePosition);
+        //prefabCurrent = Instantiate(unitPrefab, mousePosition, Quaternion.identity);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(ray))
+        prefabCurrent = Instantiate(unitPrefab);
+        //prefabCurrent = Instantiate(unitPrefab, mousePosition);
+
         Debug.Log("OnBeginDrag");
         canvasGroup.alpha = 0.6f;
         canvasGroup.blocksRaycasts = false;
@@ -35,7 +52,7 @@ public class BFUnitDragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHand
         Debug.Log("OnEndDrag");
         canvasGroup.blocksRaycasts = true;
         canvasGroup.alpha = 1f;
-        Destroy(prefabCurrent);
+        //Destroy(prefabCurrent);
     }
 
     public void OnPointerDown(PointerEventData eventData)
